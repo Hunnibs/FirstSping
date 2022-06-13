4a354e48dde24dab24f6ea0eaa0304ec648096a0
Hunnibs <cqqudgjs@hufs.ac.kr>
2022-06-14 00:33:21 +0900

    22.06.14 DB연동, AOP적용 및 입문 강의 완완강

--- src/main/java/Spring/FirstSpringProject/SpringConfig.java
+++ src/main/java/Spring/FirstSpringProject/SpringConfig.java
@@ -1,29 +1,36 @@
 package Spring.FirstSpringProject;
 
-import Spring.FirstSpringProject.repository.JdbcMemberRepository;
 import Spring.FirstSpringProject.repository.MemberRepository;
 import Spring.FirstSpringProject.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-
+    // private final DataSource dataSource;
+    // private final EntityManager em;
+    private final MemberRepository memberRepository;
+    /*
     @Autowired
-    public SpringConfig(DataSource dataSource){
+    public SpringConfig(DataSource dataSource, EntityManager em){
         this.dataSource = dataSource;
+        this.em = em;
+    }
+     */
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
+/*
     @Bean
     public MemberRepository memberRepository(){
-        return new JdbcMemberRepository(dataSource);
+        // return new MemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        // return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
+ */
 }
